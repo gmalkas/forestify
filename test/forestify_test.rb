fc02b034c4b3b7941e82da0a3c5f0483c5abe459
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-02-06 16:59:12 +0100

    Refactor tests, remove data duplication

--- test/forestify_test.rb
+++ test/forestify_test.rb
@@ -11,204 +11,109 @@ class ForestifyTest < Test::Unit::TestCase
 
 	def setup
     Tag.delete_all 
+
+    # We create some items to play with
+		# This produces the following tree
+		# { left_position, name, right_position, level }
+		# { 0, Vehicle, 9, 0 } { 10, Animal, 11, 0}
+		# { 1, Car, 4, 1 } { 5, Plane, 6, 1 } { 7, Boat, 8, 1 }
+		# { 2, Audi, 3, 2}
+
+    @vehicle = Tag.create!(name: "Vehicle")
+		@animal = Tag.create!(name: "Animal")
+		@car = Tag.create!(name: "Car", parent_id: @vehicle.id)
+		@plane = Tag.create!(name: "plane", parent_id: @vehicle.id)
+		@boat = Tag.create!(name: "Boat", parent_id: @vehicle.id)
+		@audi = Tag.create!(name: "Audi", parent_id: @car.id)
+
+    [@vehicle, @animal, @car, @plane, @boat, @audi].each { |n| n.reload }
 	end
 
 	def test_should_initialize_position_without_parent
-    tag = Tag.new(name: "Car")
-		tag.save!
-		assert_equal 0, tag.left_position
-		assert_equal 1, tag.right_position
-		assert_equal 0, tag.level
+		assert_equal 10, @animal.left_position
+		assert_equal 11, @animal.right_position
+		assert_equal 0, @animal.level
 	end
 
 	def test_should_initialize_position_with_existing_root
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-
-		animal = Tag.new(name: "Animal")
-		animal.save!
-
-		assert_equal 2, animal.left_position
-		assert_equal 3, animal.right_position
+		assert_equal 10, @animal.left_position
+		assert_equal 11, @animal.right_position
 	end
 
 	def test_should_initialize_position_with_parent
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-
-		# We have to reload the data
-		vehicle.reload
-		
-    assert_equal 3, vehicle.right_position, "Right position should have been updated"
-		assert_equal 1, car.left_position
-		assert_equal 2, car.right_position
-		assert_equal 1, car.level
+    assert_equal 4, @car.right_position, "Right position should have been updated"
+		assert_equal 2, @audi.left_position
+		assert_equal 3, @audi.right_position
+		assert_equal 2, @audi.level
 	end
 
 	def test_should_be_leaf
-    car = Tag.new(name: "Car")
-		car.save!
-
-		assert car.is_leaf?
+		assert @audi.is_leaf?
 	end
 
 	def test_should_not_be_leaf
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-
-		vehicle.reload
-
-    assert (not vehicle.is_leaf?)
+    assert (not @vehicle.is_leaf?)
 	end
 
 	def test_should_be_node
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-
-    vehicle.reload
-
-		assert vehicle.is_node?
+		assert @vehicle.is_node?
 	end
 
 	def test_should_not_be_node
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-
-		assert (not vehicle.is_node?)
+		assert (not @animal.is_node?)
 	end
 
 	def test_should_have_children_when_node
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-		plane = Tag.new(name: "plane", parent_id: vehicle.id)
-		plane.save!
-		boat = Tag.new(name: "Boat", parent_id: vehicle.id)
-		boat.save!
-
-		vehicle.reload
-
-		assert_equal 3, vehicle.children.size
+		assert_equal 4, @vehicle.children.size
 	end
 
 	def test_should_not_have_children_when_leaf
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-
-		assert_equal 0, vehicle.children.size
+		assert_equal 0, @animal.children.size
 	end
 	
 	def test_should_have_parents
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-		porsche = Tag.new(name: "Porsche", parent_id: car.id)
-		porsche.save!
-
-		vehicle.reload
-		car.reload
-
-		assert_equal Set.new([vehicle, car]), Set.new(porsche.parents)
+		assert_equal Set.new([@vehicle, @car]), Set.new(@audi.parents)
 	end
 
 	def test_should_update_nodes_when_parent_is_deleted
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-		porsche = Tag.new(name: "Porsche", parent_id: car.id)
-		porsche.save!
+    @car.destroy
     
-		car.reload
-    car.destroy
-    
-		vehicle.reload
-		porsche.reload
+		@vehicle.reload
+		@audi.reload
 
-		assert_equal 3, vehicle.right_position, "Vehicle's right position should have been updated"
-		assert_equal 2, porsche.right_position, "Porsche's right position should have been updated"
+		assert_equal 7, @vehicle.right_position, "Vehicle's right position should have been updated"
+		assert_equal 2, @audi.right_position, "Audi's right position should have been updated"
+		assert_equal 1, @audi.level, "Audi's level should have been updated"
 	end
 
 	def test_should_updates_leafs_when_node_is_deleted
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-		animal = Tag.new(name: "Animal")
-		animal.save!
-    
-    car.reload
-		car.destroy
+		@car.destroy
 
-		vehicle.reload
-		animal.reload
+		@vehicle.reload
+		@animal.reload
     
-		assert_equal 1, vehicle.right_position, "Vehicle's right position should have been updated"
-		assert_equal 2, animal.left_position, "Animal's left position should have been updated"
+		assert_equal 7, @vehicle.right_position, "Vehicle's right position should have been updated"
+		assert_equal 8, @animal.left_position, "Animal's left position should have been updated"
 	end
 
 	def test_should_have_a_parent
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-
-	  vehicle.reload
-
-		assert_equal vehicle.id, car.parent.id
+		assert_equal @vehicle.id, @car.parent.id
 	end
 
 	def test_should_not_have_a_parent
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-
-		assert_equal nil, vehicle.parent
+		assert_nil @vehicle.parent
 	end
 
 	def test_should_have_siblings
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-    plane = Tag.new(name: "Plane", parent_id: vehicle.id)
-		plane.save!
-		rocket = Tag.new(name: "Rocket", parent_id: vehicle.id)
-		rocket.save!
-
-		vehicle.reload
-		car.reload
-		plane.reload
-
-		assert_equal 2, car.siblings.size
+		assert_equal 2, @car.siblings.size
 	end
 
 	def test_should_not_have_any_siblings
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		car = Tag.new(name: "Car", parent_id: vehicle.id)
-		car.save!
-
-		vehicle.reload
-		
-		assert_equal 0, car.siblings.size
+		assert_equal 0, @audi.siblings.size
 	end
 
 	def test_should_have_siblings_when_root
-    vehicle = Tag.new(name: "Vehicle")
-		vehicle.save!
-		animal = Tag.new(name: "Animal")
-		animal.save!
-		
-		vehicle.reload
-		assert_equal 1, vehicle.siblings.size
+		assert_equal 1, @vehicle.siblings.size
 	end
 
 end
